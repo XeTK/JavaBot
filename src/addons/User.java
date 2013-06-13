ac4ab9d4e992e9741850ccaa0390776642598b79
Tom Rosier <thr2@aber.ac.uk>
2013-06-13 17:47:28 +0100

    More comments

--- src/addons/User.java
+++ src/addons/User.java
@@ -31,6 +31,12 @@ public class User
 		joins++;
 		checkDirtyness(host);
 	}
+	
+	/**
+	 * Check if the user has any drastic problems, E.G. host change or over due 
+	 * reminders. 
+	 * @param host is the user's host address so we can check if it has changed.
+	 */
 	private void checkDirtyness(String host)
 	{
 		if (reminders.size() > 0)
@@ -45,57 +51,77 @@ public class User
 		}
 	}
 	
-	public String[] getQuotes()
-	{
-		return quotes.toArray(new String[0]);
-	}
-	public void addQuote(String message)
-	{
-		quotes.add(message);
-	}
-	public void addReminder(String message)
-	{
-		reminders.add(message);
-	}
-	public void delQuote(String message)
-	{
-		quotes.remove(message);
-	}
-	public void setUser(String user)
-	{
-		this.user = user;
-	}
-	
-	
-	public String getUser()
-	{
-		return user;
-	}
-	public long getMsgSent()
-	{
-		return msgSent;
-	}
-	public Date getLastOnline()
-	{
-		return lastOnline;
-	}
-	public long getJoins()
-	{
-		return joins;
-	}
-	public long getQuits()
-	{
-		return quits;
-	}
-	public boolean isDirty()
-	{
-		return isDirty;
-	}
+	/**
+	 * Return a list of current reminders for the user, and also reset list 
+	 * ready for any new reminders that may be added.
+	 * @return's list of reminders for the user to view.
+	 */
 	public String[] getReminders()
 	{
 		String[] reminderss = reminders.toArray(new String[0]);
 		reminders = new ArrayList<String>();
 		return reminderss;
+	}
+	
+	// Return all the users quotes as an array converted from the ArrayList.
+	public String[] getQuotes()
+	{
+		return quotes.toArray(new String[0]);
+	}
+	
+	// Add a new quote to a user.
+	public void addQuote(String message)
+	{
+		quotes.add(message);
+	}
+	
+	// Add a new reminder for the user
+	public void addReminder(String message)
+	{
+		reminders.add(message);
+	}
+	
+	// Remove a quote from a user
+	public void delQuote(String message)
+	{
+		quotes.remove(message);
+	}
+	
+	// Set user id.
+	public void setUser(String user)
+	{
+		this.user = user;
+	}
+	
+	// Getters.
+	public String getUser()
+	{
+		return user;
+	}
+	
+	public long getMsgSent()
+	{
+		return msgSent;
+	}
+	
+	public Date getLastOnline()
+	{
+		return lastOnline;
+	}
+	
+	public long getJoins()
+	{
+		return joins;
+	}
+	
+	public long getQuits()
+	{
+		return quits;
+	}
+	
+	public boolean isDirty()
+	{
+		return isDirty;
 	}
 	
 }
