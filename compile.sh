20b8adaaf1324d053214635db0dfb6b7822b4ce8
Tom Rosier <tom@tomrosier.com>
2013-07-21 23:02:52 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm sources.txt
 echo Finding Files
+#http://stackoverflow.com/questions/1767384/ls-command-how-can-i-get-a-recursive-full-path-listing-one-line-per-file I love my brain explosions
 ls -R1 | while read l; do case $l in *:) d=${l%:};; "") d=;; *) echo "$d/$l";; esac; done | grep -E "\.java$" > sources.txt 
 javac -cp "gson-2.2.4.jar:javax.mail.jar" -d bin/ -sourcepath src/ @sources.txt
 echo Javabot Compiled
