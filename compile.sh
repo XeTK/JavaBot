ede3b84e1880a1786c947b0dd07cd4ce9e869623
Tom Rosier <tom@tomrosier.com>
2013-07-21 22:09:22 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm sources.txt
+echo Finding Files
 ls -R1 | while read l; do case $l in *:) d=${l%:};; "") d=;; *) echo "$d/$l";; esac; done | grep -E "\.java$" > sources.txt 
 javac -cp "gson-2.2.4.jar:javax.mail.jar" -d bin/ -sourcepath src/ @sources.txt
 echo Javabot Compiled
