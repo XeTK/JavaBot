dc45ebbc7d63657b9d31c742ce3c2650502e19f4
Tom Rosier <thr2@aber.ac.uk>
2013-07-27 17:25:17 +0100

    Heavly refactored Stats, should be much more readable now

--- src/plugin/stats/Stats.java
+++ src/plugin/stats/Stats.java
@@ -22,6 +22,26 @@ public class Stats extends Plugin
 	private final String popt_path = "options/Stat_options.json";
 	private final String log_path = "logs/%s.json";
 	
+	private final String stat_msg = "Handled %s Messages, %s users joined, "
+			+ "%s users quit, %s users kicked in the last %s!";
+	
+	private final String stat_hour = "%s in last hour: %s";
+	private final String stat_day = "%s today: %s";
+	private final String msg_lastonline = "%s was last online %s";
+	private final String msg_msgsent = "%s has sent %s messages";
+	
+	// Regex's
+	private final String rgx_stat = "\\.stats\\s(hour|day)\\s(msgsent|joins|quits|kicks)";
+	private final String rgx_time = "([0-2][0-9]):([0-5][0-9]):([0-5][0-9])";
+	private final String rgx_msgsent = "\\.msgsent\\s([\\w\\d]*)";
+	private final String rgx_lastonline = "\\.lastonline\\s([\\w\\d]*)";
+	
+	private final Pattern dot_stat = Pattern.compile(rgx_stat, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	private final Pattern ptn_stat = Pattern.compile(rgx_time, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	private final Pattern dot_msgsent = Pattern.compile(rgx_msgsent, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	private final Pattern dot_lastonline = Pattern.compile(rgx_lastonline, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	private final IRC irc = IRC.getInstance();
+	
 	private String save_path = new String();
 	private String opt_path = new String();
 	private String channel_name = new String();
@@ -51,29 +71,46 @@ public class Stats extends Plugin
 
 	public void onTime() throws Exception
 	{
-		IRC irc = IRC.getInstance();
 		String ti = new SimpleDateFormat("HH:mm:ss").format(new Date());
 		
-		Matcher m = 
-				Pattern.compile("([0-2][0-9]):([0-5][0-9]):([0-5][0-9])",
-						Pattern.CASE_INSENSITIVE | Pattern.DOTALL).matcher(ti);
+		Matcher m = ptn_stat.matcher(ti);
 		if (m.find())
 		{
 			String hour = m.group(1);
 			String min = m.group(2);
 			String sec = m.group(3);			
 			
-			if (options.isHour_Stats())
-				if (min.equals("59")&&sec.equals("59"))
-					if (today.getHour().getMsgSent() != 0)
-						irc.sendPrivmsg(channel_name, "Hourly Stats: Messages Sent : " + today.getHour().getMsgSent() + "| Users joined :" + today.getHour().getJoins() +"| Users left : " + today.getHour().getQuits() + "| Users kicked" + today.getHour().getKicks());
+			if (options.isHour_Stats()&&
+					min.equals("59")&&sec.equals("59")&&
+					today.getHour().getMsgSent() != 0)
+			{
+				StatHour thour = today.getHour();
+				
+				String msg = String.format(stat_msg, 
+						thour.getMsgSent(),
+						thour.getJoins(),
+						thour.getQuits(),
+						thour.getKicks(),
+						"hour"
+						);
+				
+				irc.sendActionMsg(channel_name, msg);
+			}
 			
 			if (hour.equals("00")&&min.equals("00")&&sec.equals("00"))
 			{
-				if (options.isDay_Stats())
-					if (today.msgsSent() != 0)
-						irc.sendPrivmsg(channel_name, "I has handled, " + today.msgsSent() + " Messages, " + today.joins() + "  Users Join, " + today.quits() + " User left and " + today.kicks() + " users kicked!");
-
+				if (options.isDay_Stats()&&today.msgsSent() != 0)
+				{
+					String msg = String.format(stat_msg,
+							today.msgsSent(),
+							today.joins(),
+							today.quits(),
+							today.kicks(),
+							"day"
+							);
+					
+					irc.sendActionMsg(channel_name, msg);
+				}
 				today = new StatDay();
 			}
 			
@@ -99,8 +136,6 @@ public class Stats extends Plugin
 			
 			if (message.charAt(message.length() - 1 ) == ' ')
 				message = message.substring(0, message.length() -1);
-	
-			IRC irc = IRC.getInstance();
 			
 			UserList ul = UserList.getInstance();
 			
@@ -109,70 +144,89 @@ public class Stats extends Plugin
 			
 			today.incMsgSent(user);
 			
-			if (message.matches("\\.msgsent [A-Za-z0-9#]+$"))
+			Matcher m;
+			
+			m = dot_msgsent.matcher(message);
+			if (m.matches())
 			{
-				String[] t = message.split(" ");
-				if ((t.length > 0||t[1] != null)&&ul.getUser(t[1]) != null)
-				{
-					irc.sendPrivmsg(channel, t[1] + ": Messages Sent = " + ul.getUser(t[1]).getMsgSent() + "!");
-				}
+				String tuser = m.group(1);
+				long msgsent = ul.getUser(tuser).getMsgSent();
+				String msg = String.format(msg_msgsent, tuser, msgsent);
+				irc.sendPrivmsg(channel, msg);
 			}
-			else if (message.matches("\\.lastonline [A-Za-z0-9#]+$"))
+
+			m = dot_lastonline.matcher(message);
+			if (m.matches())
 			{
-				String[] t = message.split(" ");
-				if ((t.length > 0||t[1] != null)&&ul.getUser(t[1]) != null)
-				{
-					irc.sendPrivmsg(channel, t[1] + ": Last Online = " + new SimpleDateFormat("yyyy/MM/dd HH:mm").format(ul.getUser(t[1]).getLastOnline()) + "!");
-				}
+				String tuser = m.group(1);
+				Date lastonline = ul.getUser(tuser).getLastOnline();
+				String format = "HH:mm dd:MM:yyyy";
+				String date = new SimpleDateFormat(format).format(lastonline);
+				String msg = String.format(msg_lastonline, tuser, date);
+				irc.sendPrivmsg(channel, msg);
 			}
-			else if (message.matches("\\.stats (hour|day) (msgsent|joins|quits|kicks)"))
+
+			m = dot_stat.matcher(message);
+			if (m.find())
 			{
-				Matcher m =	Pattern.compile("\\.stats (hour|day) (msgsent|joins|quits|kicks)",
-								Pattern.CASE_INSENSITIVE | Pattern.DOTALL).matcher(message);
-				if (m.find())
+				String t1 = m.group(1);
+				String cm = m.group(2);
+				
+				String msg = new String();
+				String type = new String();
+				int count = 0;
+				
+				if (t1.equals("hour"))
 				{
-					String t1 = m.group(1);
-					String cm = m.group(2);
+					StatHour hour = today.getHour();
 					
-					if (t1.equals("hour"))
+					if (cm.equals("msgsent"))
 					{
-						if (cm.equals("msgsent"))
-						{
-							irc.sendPrivmsg(channel, "Messages sent in the last hour : " + today.getHour().getMsgSent());
-						}
-						else if (cm.equals("joins"))
-						{
-							irc.sendPrivmsg(channel, "Users joined in the last hour : " + today.getHour().getJoins());
-						}
-						else if (cm.equals("quits"))
-						{
-							irc.sendPrivmsg(channel, "Users quit in the last hour : " + today.getHour().getQuits());
-						}
-						else if (cm.equals("kicks"))
-						{
-							irc.sendPrivmsg(channel, "Users kicked in the last hour : " + today.getHour().getKicks());
-						}
+						type = "Messages";
+						count = hour.getMsgSent();
 					}
-					else if (t1.equals("day"))
+					else if (cm.equals("joins"))
 					{
-						if (cm.equals("msgsent"))
-						{
-							irc.sendPrivmsg(channel, "Messages sent today : " + today.msgsSent());
-						}
-						else if (cm.equals("joins"))
-						{
-							irc.sendPrivmsg(channel, "Users joined today : " + today.joins());
-						}
-						else if (cm.equals("quits"))
-						{
-							irc.sendPrivmsg(channel, "Users quit today : " + today.quits());
-						}
-						else if (cm.equals("kicks"))
-						{
-							irc.sendPrivmsg(channel, "Users kicked today : " + today.kicks());
-						}
+						type = "Joins";
+						count = hour.getJoins();
 					}
+					else if (cm.equals("quits"))
+					{
+						type = "Quits";
+						count = hour.getQuits();
+					}
+					else if (cm.equals("kicks"))
+					{
+						type = "Kicks";
+						count = hour.getKicks();
+					}
+					msg = String.format(stat_hour, type,count);
 				}
+				else if (t1.equals("day"))
+				{
+					if (cm.equals("msgsent"))
+					{
+						type = "Messages";
+						count = today.msgsSent();
+					}
+					else if (cm.equals("joins"))
+					{
+						type = "Joins";
+						count = today.joins();
+					}
+					else if (cm.equals("quits"))
+					{
+						type = "Quits";
+						count = today.quits();
+					}
+					else if (cm.equals("kicks"))
+					{
+						type = "Kicks";
+						count = today.kicks();
+					}
+					msg = String.format(stat_day, type, count);
+				}
+				irc.sendPrivmsg(channel, msg);
 			}
 		}
 	}
